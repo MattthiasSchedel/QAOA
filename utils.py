5146f965ad75bfc7db22fa8ca232f1627345fb2b
MattthiasSchedel <matthias.anton.schedel@gmail.com>
2024-05-16 16:16:18 +0200

    initial commit

---
+++ utils.py
@@ -0,0 +1,30 @@
+custom_colors = {
+    "red": {
+        "light": "#F7CECC",
+        "dark": "#b85450"
+    },
+    "green": {
+        "light": "#D5E7D4",
+        "dark": "#81B365"
+    },
+    "blue": {
+        "light": "#DAE8FC",
+        "dark": "#6C8EBF"
+    },
+    "yellow": {
+        "light": "#FFF1CC",
+        "dark": "#D6B656"
+    },
+    "orange": {
+        "light": "#FFE6CC",
+        "dark": "#D79A01"
+    },
+    'grey': {
+        'light': '#F5F5F5',
+        'dark': '#666666'
+    }
+}
+
+
+if __name__ == "__main__":
+    print(custom_colors)
