9a230cd1b084f7e88c152dea845087027e96ed16
MattthiasSchedel <matthias.anton.schedel@gmail.com>
2024-05-16 18:40:50 +0200

    another test

--- setup.sh
+++ setup.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 #SBATCH --gres=gpu:0
-#SBATCH --partition=gpuA100 
+#SBATCH --partition=gpuA100
 #SBATCH --time=02:15:00
 #SBATCH --job-name=qaoa_setup
 #SBATCH --output=qaoa_setup.out
- 
+
 # Set up environment
 uenv verbose cuda-12.3.2 cudnn-12.x-9.0.0 
 uenv miniconda3-py310
-conda create -n qaoa -c   qiskit-aer-gpu  qiskit  qiskit-optimization  scipy  matplotlib  numpy  networkx -y
+
+# Create conda environment
+conda create -n qaoa -y
+
+# Activate conda environment
+source activate qaoa
+
+# Install packages
+conda install -c conda-forge qiskit qiskit-optimization scipy matplotlib numpy networkx -y
+
+# Install qiskit-aer with GPU support
+pip install qiskit-aer-gpu
