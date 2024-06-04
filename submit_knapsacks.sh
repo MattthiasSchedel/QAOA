232a9d09259e6eb27d846cfe4235e329b0a9de62
Matthias Anton Schedel <matsche@ux.uis.no>
2024-06-04 11:14:21 +0200

    knapsack tests

--- submit_knapsacks.sh
+++ submit_knapsacks.sh
@@ -11,30 +11,30 @@ max_elements=10
 # test the maxcut problem 
 min_number_of_layers=1
 max_number_of_layers=30
-number_of_repetitions=100
+number_of_repetitions=2
 
 # wait for the problem to be generated
-sleep 30
 
+
+#for number_of_layers in $(seq $min_number_of_layers $max_number_of_layers)
+#do
+#    for number_of_elements in $(seq $min_elements $max_elements)
+#    do
+#        problem_name="knapsack_problem_${number_of_elements}_.txt"
+#        echo $problem_name
+#        sbatch test_problem_optimal.sh "$problem_name" $number_of_layers $number_of_repetitions 100 -2
+#    done
+#done
+#sleep 30
 for number_of_layers in $(seq $min_number_of_layers $max_number_of_layers)
 do
     for number_of_elements in $(seq $min_elements $max_elements)
-    do
-        problem_name="knapsack_problem_${number_of_elements}_$.txt"
+    do	
+	echo $number_of_elements $number_of_layers
+        problem_name="knapsack_problem_${number_of_elements}_.txt"
         echo $problem_name
-        sbatch test_problem_optimal.sh "$problem_name" $number_of_layers $number_of_repetitions
-    done
-done
-
-for number_of_layers in $(seq $min_number_of_layers $max_number_of_layers)
-do
-    for number_of_elements in $(seq $min_elements $max_elements)
-    do
-        for i in $(seq 1 $number_of_repetitions)
-        do
-            problem_name="knapsack_problem_${number_of_elements}_$.txt"
-            echo $problem_name
-            sbatch test_problem_noisy.sh "$problem_name" $number_of_layers $i
-        done
+        sbatch test_problem_noisy.sh "$problem_name" $number_of_layers $number_of_repetitions
+	
+	
     done
 done
